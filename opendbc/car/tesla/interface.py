a10bc4226b8a3c1a809c5703651da2268349f660
t-jonesy <me@taylorjones.engineer>
2024-10-24 17:36:05 -0400

    stock acc toggle

--- opendbc/car/tesla/interface.py
+++ opendbc/car/tesla/interface.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from panda import Panda
+from openpilot.common.params import Params
 from opendbc.car import get_safety_config, structs
 from opendbc.car.interfaces import CarInterfaceBase
 from opendbc.car.tesla.values import CAR
@@ -14,7 +15,12 @@ class CarInterface(CarInterfaceBase):
     ret.dashcamOnly = False
 
     # Not merged yet
+    params = Params()
+    stock_acc = params.get_bool("StockTaccEnabledToggle")
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.tesla)]
+    if not stock_acc:
+      flags |= Panda.FLAG_TESLA_LONG_CONTROL
+    ret.openpilotLongitudinalControl = not stock_acc
     ret.safetyConfigs[0].safetyParam |= Panda.FLAG_TESLA_LONG_CONTROL
 
     ret.steerLimitTimer = 1.0
@@ -23,6 +29,5 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = structs.CarParams.SteerControlType.angle
     ret.radarUnavailable = True
 
-    ret.openpilotLongitudinalControl = True
 
     return ret
